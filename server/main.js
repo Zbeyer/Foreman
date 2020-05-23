235d685bfc4ef2fa6ffe83c74a67c59d46d224c4
Zachary Beyer <zack@boomlearning.com>
2020-05-23 14:21:37 -0700

    Completely axed the starter project and fixed eager loading

--- server/main.js
+++ server/main.js
@@ -1,9 +1,4 @@
-import { Meteor } from 'meteor/meteor';
-
-
-// https://www.meteor.com/tutorials/blaze/collections
-import '../imports/api/todo/tasks.js';
-
-Meteor.startup(() => {
-  // code to run on server at startup
+Meteor.startup(() =>
+{
+    // code to run on server at startup
 });
